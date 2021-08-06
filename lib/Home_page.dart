33316a211e9e271b73ad142001b6b84ef86e77a6
Moin4488 <moinmulla4488@gmail.com>
2021-08-06 14:16:50 +0530

    second commit

---
+++ lib/Home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int day = 30;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to $day days of Flutter'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
